58fd222474ea8fe1dd28a45ae50c088d80e407a3
doriorio <itsdori@gmail.com>
2019-09-26 21:03:18 -0700

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -3,21 +3,32 @@ console.log("connected!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+//var cardOne = cards[0];
+//cardsInPlay.push(cardOne);
+//console.log("User flipped" + cardOne);
 
-console.log("User flipped " + cardOne);
+//var cardTwo = cards[2];
+//cardsInPlay.push(cardTwo);
+//console.log("User flipped" + cardTwo);
 
-console.log("User flipped " + cardTwo);
 
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
     if (cardsInPlay[0] === cardsInPlay[1]){
         alert("You found a match!");
-    }
-    else {
+    } else {
         alert("Sorry try again!");
     }
 }
+
+
+
+function flipCard(cardId) {
+    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    checkForMatch();
+}
+
+flipCard(4);
+
+
